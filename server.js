6c1d81eb620f88d17ad8ab7d59e3013bd3e37d61
Ubuntu <ubuntu@ip-172-31-28-232.ec2.internal>
2025-04-01 12:31:18 +0000

    cors update to include vite

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ const app = express();
 // Apply CORS middleware with proper configuration ONCE
 app.use(
   cors({
-    origin: 'http://127.0.0.1:5173',
+    origin: ['http://127.0.0.1:5173',
+            'http://localhost:5173'
+         ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
