0573a919dabad0988ab3910a388e63c51ae8064d
johnbekele <yohansdemisie@gmail.com>
2025-04-01 14:26:33 +0200

    Update the origin to include your HTTPS domain  and add Security headers middleware for https redirect

--- index.js
+++ index.js
@@ -1,299 +1,291 @@
-import express from "express";
-import bodyParser from "body-parser";
-import pg from "pg";
-import dotenv from "dotenv";
-import axios from "axios";
-import bcrypt from "bcrypt";
-import session from "express-session";
+// import express from "express";
+// import bodyParser from "body-parser";
+// import pg from "pg";
+// import dotenv from "dotenv";
+// import axios from "axios";
+// import bcrypt from "bcrypt";
+// import session from "express-session";
 
+// dotenv.config();
+// // const BASE_URL =
+// //   "https://generativeai.googleapis.com/v1beta3/models/gemini-1.5-flash:generateText";
+// // const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-dotenv.config();
-// const BASE_URL =
-//   "https://generativeai.googleapis.com/v1beta3/models/gemini-1.5-flash:generateText";
-// const { GoogleGenerativeAI } = require("@google/generative-ai");
+// const port = process.env.PORT || 8080;
+// const app = express();
 
-const port = process.env.PORT || 8080;
-const app = express();
+// //middleware configuration
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static("public"));
+// app.set("view engine", "ejs");
 
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//       maxAge: 60000 * 24 * 7, // 7 days
+//       secure: false,
+//     },
+//   })
+// );
 
+// app.use(passport.initialize());
+// app.use(passport.session());
 
+// const db = new pg.Client({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "books",
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT,
+// });
+// db.connect();
 
-//middleware configuration
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.set("view engine", "ejs");
+// //Join query to database fetching posted books with user data
+// async function getbooks() {
+//   const result = await db.query(
+//     "SELECT * FROM fav_list INNER JOIN users ON fav_list.user_id = users.id;"
+//   );
+//   let Books = [];
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      maxAge: 60000 * 24 * 7, // 7 days
-      secure: false,
-    },
-  })
-);
+//   result.rows.forEach((book) => {
+//     Books.push(book);
+//   });
+//   return Books;
+// }
 
-app.use(passport.initialize());
-app.use(passport.session());
+// //middleware to check user Authentication
+// function AuthUser(req, res, next) {
+//   if (
+//     req.isAuthenticated() &&
+//     (req.user.strategy === "local" || req.user.strategy === "google")
+//   ) {
+//     return next();
+//   } else {
+//     res.redirect("/login");
+//   }
+// }
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "books",
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-});
-db.connect();
+// //middleware to protected routes
 
-//Join query to database fetching posted books with user data
-async function getbooks() {
-  const result = await db.query(
-    "SELECT * FROM fav_list INNER JOIN users ON fav_list.user_id = users.id;"
-  );
-  let Books = [];
+// app.use("/protected", AuthUser);
 
-  result.rows.forEach((book) => {
-    Books.push(book);
-  });
-  return Books;
-}
+// app.get("/", async (req, res) => {
+//   const bookName = "rich dad poor dad";
 
-//middleware to check user Authentication
-function AuthUser(req, res, next) {
-  if (
-    req.isAuthenticated() &&
-    (req.user.strategy === "local" || req.user.strategy === "google")
-  ) {
-    return next();
-  } else {
-    res.redirect("/login");
-  }
-}
+//   // const response = await axios.get(
+//   //   `https://www.googleapis.com/books/v1/volumes?q=${bookName}&key=${GOOGLE_API}`
+//   // );
+//   // const results = response.data;
+//   // console.log(results.imageLinks.smallThumbnail);
+//   const books = await getbooks();
 
-//middleware to protected routes
+//   const userphoto = req.user ? req.user.picture : null;
 
-app.use("/protected", AuthUser);
+//   res.render("pages/home", {
+//     books: books,
+//     user: req.user,
+//   });
+// });
 
-app.get("/", async (req, res) => {
-  const bookName = "rich dad poor dad";
+// //search books route
+// // Search books route
+// app.get("/search", async (req, res) => {
+//   const query = req.query.q; // Get search query from request
+//   if (!query) {
+//     return res
+//       .status(400)
+//       .json({ success: false, message: "Query is required" });
+//   }
 
-  // const response = await axios.get(
-  //   `https://www.googleapis.com/books/v1/volumes?q=${bookName}&key=${GOOGLE_API}`
-  // );
-  // const results = response.data;
-  // console.log(results.imageLinks.smallThumbnail);
-  const books = await getbooks();
+//   try {
+//     // Perform a case-insensitive search for books by title or author
+//     const searchQuery = `
+//       SELECT book_title, book_author, book_comment, book_rating
+//       FROM fav_list
+//       WHERE LOWER(book_title) LIKE $1 OR LOWER(book_author) LIKE $1
+//     `;
+//     const results = await db.query(searchQuery, [`%${query.toLowerCase()}%`]);
 
-  const userphoto = req.user ? req.user.picture : null;
+//     if (results.rowCount > 0) {
+//       res.status(200).json({ success: true, data: results.rows });
+//     } else {
+//       res.status(404).json({ success: false, message: "No books found" });
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ success: false, message: "Server error" });
+//   }
+// });
 
-  res.render("pages/home", {
-    books: books,
-    user: req.user,
-  });
-});
+// // AUth local route
+// app.get("/login", (req, res) => {
+//   res.render("pages/auth/login", { message: null });
+// });
 
-//search books route
-// Search books route
-app.get("/search", async (req, res) => {
-  const query = req.query.q; // Get search query from request
-  if (!query) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Query is required" });
-  }
+// app.post(
+//   "/login",
+//   passport.authenticate("local", {
+//     successRedirect: "/protected/books/add",
+//     failureRedirect: "/login",
+//   })
+// );
 
-  try {
-    // Perform a case-insensitive search for books by title or author
-    const searchQuery = `
-      SELECT book_title, book_author, book_comment, book_rating 
-      FROM fav_list 
-      WHERE LOWER(book_title) LIKE $1 OR LOWER(book_author) LIKE $1
-    `;
-    const results = await db.query(searchQuery, [`%${query.toLowerCase()}%`]);
+// app.get("/register", (req, res) => {
+//   res.render("pages/auth/register", { message: null });
+// });
 
-    if (results.rowCount > 0) {
-      res.status(200).json({ success: true, data: results.rows });
-    } else {
-      res.status(404).json({ success: false, message: "No books found" });
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ success: false, message: "Server error" });
-  }
-});
+// app.post("/register", async (req, res) => {
+//   const { username, email, password, terms } = req.body;
+//   const query =
+//     "INSERT INTO users (username, email, password , terms) VALUES ($1, $2, $3, $4) RETURNING *;";
+//   try {
+//     const results = await db.query("SELECT email FROM users WHERE email=$1 ", [
+//       email,
+//     ]);
 
-// AUth local route
-app.get("/login", (req, res) => {
-  res.render("pages/auth/login", { message: null });
-});
+//     if (results.rows.length > 0) {
+//       res.render("pages/auth/register", {
+//         message: "email alredy used please log in  ",
+//       });
+//     } else {
+//       bcrypt.hash(password, saltRounds, async (err, hash) => {
+//         if (err) {
+//           console.error(err);
+//           res.render("pages/auth/register", {
+//             message:
+//               "An error occurred while processing your request. Please try again.",
+//           });
+//         } else {
+//           console.log(hash);
 
-app.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/protected/books/add",
-    failureRedirect: "/login",
-  })
-);
+//           const result = await db.query(query, [username, email, hash, terms]);
 
-app.get("/register", (req, res) => {
-  res.render("pages/auth/register", { message: null });
-});
+//           if (result.rowCount > 0) {
+//             res.render("pages/auth/login", {
+//               message: "registeration succesfull log in to your account ",
+//             });
+//           } else {
+//             res.render("pages/auth/register", {
+//               message: "registration faild",
+//             });
+//           }
+//         }
+//       });
+//     }
+//   } catch (err) {
+//     console.error(err);
+//   }
+// });
 
-app.post("/register", async (req, res) => {
-  const { username, email, password, terms } = req.body;
-  const query =
-    "INSERT INTO users (username, email, password , terms) VALUES ($1, $2, $3, $4) RETURNING *;";
-  try {
-    const results = await db.query("SELECT email FROM users WHERE email=$1 ", [
-      email,
-    ]);
+// //Auth google route
 
-    if (results.rows.length > 0) {
-      res.render("pages/auth/register", {
-        message: "email alredy used please log in  ",
-      });
-    } else {
-      bcrypt.hash(password, saltRounds, async (err, hash) => {
-        if (err) {
-          console.error(err);
-          res.render("pages/auth/register", {
-            message:
-              "An error occurred while processing your request. Please try again.",
-          });
-        } else {
-          console.log(hash);
+// app.get(
+//   "/auth/google",
+//   passport.authenticate("google", {
+//     scope: ["profile", "email"],
+//   })
+// );
 
-          const result = await db.query(query, [username, email, hash, terms]);
+// app.get(
+//   "/auth/google/protected/books/add",
+//   passport.authenticate("google", {
+//     successRedirect: "/protected/books/add",
+//     failureRedirect: "/login",
+//   })
+// );
 
-          if (result.rowCount > 0) {
-            res.render("pages/auth/login", {
-              message: "registeration succesfull log in to your account ",
-            });
-          } else {
-            res.render("pages/auth/register", {
-              message: "registration faild",
-            });
-          }
-        }
-      });
-    }
-  } catch (err) {
-    console.error(err);
-  }
-});
+// //log out
 
-//Auth google route
+// app.get("/logout", (req, res) => {
+//   req.logout((err) => {
+//     if (err) console.log(err);
+//     res.redirect("/");
+//   });
+// });
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
+// //bookStore routes
+// app.get("/protected/books/add", (req, res) => {
+//   console.log(req.newuser);
+//   res.render("pages/books/add-book");
+// });
 
-app.get(
-  "/auth/google/protected/books/add",
-  passport.authenticate("google", {
-    successRedirect: "/protected/books/add",
-    failureRedirect: "/login",
-  })
-);
+// app.post("/protected/books/add", async (req, res) => {
+//   const userId = req.user.id;
+//   const bookId = 101; //will be updated by the API ID
+//   const {
+//     bookTitle,
+//     author,
+//     startDate,
+//     endDate,
+//     favoritePoints,
+//     memories,
+//     rating,
+//   } = req.body;
+//   const query = `
+//     INSERT INTO fav_list (book_title, book_author, start_date, end_date, book_comment, book_memories, book_rating, book_id, user_id)
+//     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+//     RETURNING *;
+//   `;
+//   try {
+//     const result = await db.query(query, [
+//       bookTitle,
+//       author,
+//       startDate,
+//       endDate,
+//       favoritePoints,
+//       memories,
+//       rating,
+//       bookId,
+//       userId,
+//     ]);
 
-//log out
+//     if (result.rowCount > 0) {
+//       res.status(201).json({ success: true, data: result.rows[0] });
+//       res.render("pages/books/add-book", {
+//         message: "book added successfully",
+//       });
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ success: false, message: "Error adding favorite" });
+//   }
+// });
 
-app.get("/logout", (req, res) => {
-  req.logout((err) => {
-    if (err) console.log(err);
-    res.redirect("/");
-  });
-});
+// //Ai integration
 
-//bookStore routes
-app.get("/protected/books/add", (req, res) => {
-  console.log(req.newuser);
-  res.render("pages/books/add-book");
-});
+// app.get("/ai", async (req, res) => {
+//   const data = await getbooks();
+//   const formatedData = data
+//     .map(
+//       (book) => `
+//  id:${book.id} ,
+//  name:${book.bookTitle},
+//  author:${book.bookAuthor},
+//  comment:${book.book_comment},
+//  rating:${book.book_rating} `
+//     )
+//     .join("\n");
+//   const prompt = `Analyze the following data:\n${formatedData}\n\n and give the best fiction book with good rating  `;
 
-app.post("/protected/books/add", async (req, res) => {
-  const userId = req.user.id;
-  const bookId = 101; //will be updated by the API ID
-  const {
-    bookTitle,
-    author,
-    startDate,
-    endDate,
-    favoritePoints,
-    memories,
-    rating,
-  } = req.body;
-  const query = `
-    INSERT INTO fav_list (book_title, book_author, start_date, end_date, book_comment, book_memories, book_rating, book_id, user_id)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-    RETURNING *;
-  `;
-  try {
-    const result = await db.query(query, [
-      bookTitle,
-      author,
-      startDate,
-      endDate,
-      favoritePoints,
-      memories,
-      rating,
-      bookId,
-      userId,
-    ]);
+//   // AI api integration tests
+//   try {
+//     const result = await model.generateContent(prompt);
 
-    if (result.rowCount > 0) {
-      res.status(201).json({ success: true, data: result.rows[0] });
-      res.render("pages/books/add-book", {
-        message: "book added successfully",
-      });
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ success: false, message: "Error adding favorite" });
-  }
-});
+//     const AIresponse = result.response.candidates[0].output;
 
-//Ai integration
+//     console.log(AIresponse);
+//     res.status(200).json({ success: true, message: AIresponse });
+//   } catch (error) {
+//     console.error(error);
+//     res
+//       .status(500)
+//       .json({ success: false, message: "Error generating AI response" });
+//   }
+// });
 
-app.get("/ai", async (req, res) => {
-  const data = await getbooks();
-  const formatedData = data
-    .map(
-      (book) => `
- id:${book.id} ,
- name:${book.bookTitle},
- author:${book.bookAuthor},
- comment:${book.book_comment},
- rating:${book.book_rating} `
-    )
-    .join("\n");
-  const prompt = `Analyze the following data:\n${formatedData}\n\n and give the best fiction book with good rating  `;
-
-  // AI api integration tests
-  try {
-    const result = await model.generateContent(prompt);
-
-    const AIresponse = result.response.candidates[0].output;
-
-    console.log(AIresponse);
-    res.status(200).json({ success: true, message: AIresponse });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ success: false, message: "Error generating AI response" });
-  }
-});
-
-
-
-
-
-app.listen(port, () => {
-  console.log(`Server is running on port http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port http://localhost:${port}`);
+// });
