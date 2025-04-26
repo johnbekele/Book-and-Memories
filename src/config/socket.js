fbde2e4e7cf3aa679dffaa81ccae75f28240ece0
johnbekele <yohansdemisie@gmail.com>
2025-04-26 09:51:44 +0200

    fix on the front url

--- src/config/socket.js
+++ src/config/socket.js
@@ -4,7 +4,7 @@ import messageController from '../controllers/messageController.js';
 const configureSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: process.env.CLIENT_URL || 'http://localhost:5173',
+      origin: process.env.PROD_FRONTEND_URL || 'http://localhost:5173',
       methods: ['GET', 'POST'],
       credentials: true,
       allowedHeaders: ['Content-Type', 'Authorization'],
